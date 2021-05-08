26984e49201c17fc8a1a0f3ff33c48f757b7da7d
vishal katkar <vishalkatkar@seniority.in>
2021-05-08 14:49:21 +0530

    add blood group

--- src/constants.js
+++ src/constants.js
@@ -6,8 +6,11 @@ export const COVID_HELP_MAIN_CATEGORY = [
 ];
 
 export const BLOOD_GROUP = [
-  { title: "A", value: "A" },
-  { title: "B", value: "B" },
-  { title: "AB", value: "AB" },
+  { title: "A+", value: "A+" },
+  { title: "A-", value: "A-" },
+  { title: "B+", value: "B+" },
+  { title: "B-", value: "B-" },
+  { title: "AB+", value: "AB+" },
+  { title: "AB", value: "AB-" },
   { title: "O", value: "O" },
-]
+];
